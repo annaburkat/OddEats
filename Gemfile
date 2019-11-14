0a4ae70a9cd288fd3286b559783473cd2478f8cc
EC2 Default User <ec2-user@ip-172-31-89-111.ec2.internal>
2019-11-14 21:27:34 +0000

    static pages

--- Gemfile
+++ Gemfile
@@ -14,6 +14,10 @@ gem 'sqlite3', '~>1.3.0'
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+# Use Bootstrap
+gem 'bootstrap-sass', '~> 3.4.1'
+# Use SCSS for stylesheets
+gem 'sassc-rails', '>= 2.1.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :vouchers
-  get 'static_pages/home'
+  
+  root 'static_pages#home'
 
-  get 'static_pages/about'
+  get '/about' => 'static_pages#about'
 
-  get 'static_pages/contact'
+  get '/contact' => 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
