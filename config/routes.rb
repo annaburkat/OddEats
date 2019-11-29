053d9de48a0308f4860c85def09a26eac316e1eb
EC2 Default User <ec2-user@ip-172-31-89-111.ec2.internal>
2019-11-29 04:45:54 +0000

    cart and login attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'cart/index'
+
   resources :restaurants
   resources :vouchers
   
@@ -9,6 +11,14 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
 
   get '/contact' => 'static_pages#contact'
+  
+  get '/login', to: 'user#login' 
+
+  get '/logout', to: 'user#logout'
+
+  get '/cart/:id', to: 'cart#add'
+  
+
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
